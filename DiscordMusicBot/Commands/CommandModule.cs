b253ccf673dbe1f7c551c7c52734442f0248c60b
kirik <kirik@DESKTOP-B789L69>
2024-01-18 19:46:09 +0100

    Add project files.

---
+++ DiscordMusicBot/Commands/CommandModule.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Lavalink;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMusicBot.Commands
+{
+    public class CommandModule : BaseCommandModule
+    {
+        [Command("greet")]
+        public async Task GreetEveryone(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Hello @all");
+        }
+
+        [Command("play")]
+        public async Task PlayMusic (CommandContext ctx, string link)
+        {
+            var userVC = ctx.Member.VoiceState.Channel; //returns a voicechannel if user is sitting in one
+            var lavalinkInstance = ctx.Client.GetLavalink();
+
+            if(ctx.Member.VoiceState == null || userVC == null)
+            {
+                await ctx.Channel.SendMessageAsync("Please enter a VC!");
+                return;
+            }
+
+
+            // found a bug, can't find any nodes
+
+            if(lavalinkInstance.ConnectedNodes.Any())
+            {
+                await ctx.Channel.SendMessageAsync("Lavalink is dead or not connected");
+                return;
+            }
+
+            // bug ends
+
+
+
+            if(userVC.Type != DSharpPlus.ChannelType.Voice) // if uservc is not in a valid voicechannel
+            {
+                await ctx.Channel.SendMessageAsync("Please enter a valid VC!");
+                return;
+            }
+
+            // connection to a voice channel
+            var node = lavalinkInstance.ConnectedNodes.Values.First();
+            await node.ConnectAsync(userVC);
+
+            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild); //get guild connection
+
+            if (conn == null)
+            {
+                await ctx.Channel.SendMessageAsync("Lavalink failed to connect");
+                return;
+            }
+        }
+    }
+}
---
+++ DiscordMusicBot/Program.cs
@@ -0,0 +1,87 @@
+﻿using DiscordMusicBot.Commands;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+
+namespace DiscordMusicBot
+{
+    public class Program
+    {
+        public static DiscordClient Client { get; private set; }
+        public static InteractivityExtension Interactivity { get; private set; }
+        public static CommandsNextExtension Commands { get; private set; }
+
+        public static async Task Main(string[] args)
+        {
+            string path = new Program().GetPath();
+            Dictionary<string, string> Token_ = new Dictionary<string, string>();
+            if (path != null)
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string json = reader.ReadToEnd();
+                    Token_ = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                }
+
+            }
+
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = Token_["Token"],
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            });
+
+            commands.RegisterCommands<CommandModule>();
+
+            var endpoint = new ConnectionEndpoint()
+            {
+                Hostname = "lava.dcmusic.ca",
+                Port = 443,
+                Secured = true
+            };
+
+            var lavalinkConfig = new LavalinkConfiguration()
+            {
+                Password = "youshallnotpass",
+                RestEndpoint = endpoint,
+                SocketEndpoint = endpoint,
+            };
+
+            var lavalink = discord.UseLavalink();
+
+            await discord.ConnectAsync();
+            await lavalink.ConnectAsync(lavalinkConfig);
+
+            await Task.Delay(-1);
+        }
+
+
+        public string GetPath()
+        {
+            DirectoryInfo di = new DirectoryInfo(AppContext.BaseDirectory);
+            string path = di.Parent.Parent.Parent.FullName;
+            string checkpath = path.Substring(path.Length - 15);
+            if (checkpath == "DiscordMusicBot" && Path.Exists(Path.Combine(path, "JsonData", "Config.json")))
+            {
+                return Path.Combine(path, "JsonData", "Config.json");
+            }
+            return null;
+        }
+
+
+
+    }
+}
